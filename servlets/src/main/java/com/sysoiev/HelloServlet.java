f105580a6b069fa2b66094a5e543140725def29e
Bada1208 <sysoievbogdan@gmail.com>
2020-03-24 07:55:42 +0100

    Initial commit and added first test sevlet class

---
+++ servlets/src/main/java/com/sysoiev/HelloServlet.java
@@ -0,0 +1,25 @@
+package com.sysoiev;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //first of all need to delete index.jsp file, in order to tomcat read from the sevlet straightly  
+        response.setContentType("text/html");
+        PrintWriter printWriter = response.getWriter();
+        printWriter.println("<h2>Hello from HelloServlet</h2>");
+        printWriter.close();
+
+    }
+}
